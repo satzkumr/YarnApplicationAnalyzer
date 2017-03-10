aa7dbe31ed05f342b6dc76c9f036533306dfa192
satzkumr <mrsathishkumar12@gmail.com>
2017-03-11 02:24:19 +0530

    Initial Version-0.01

---
+++ bin/appanalyzer.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#Sourcing environment variables
+
+ANALYZER_HOME=/home/mapr/myProjects/YarnApplicationAnalyzer
+
+source $ANALYZER_HOME/etc/analyzerEnv.sh
+
+
+#Usage Function
+
+usage()
+{
+        echo "USAGE: appAnalyzer.sh <application ID> <Logfile>"
+}
+
+
+#Application ID Validator
+
+validate_id()
+{
+
+ 	appId=$1
+	printf "\n\n"
+        echo "-------------------------------------------------------------------------------------------------"
+	echo "Running check for Application: $1 in logs at $2"
+	echo "-------------------------------------------------------------------------------------------------"
+
+}
+
+#Aalyzer Core functions
+
+getContainers()
+{
+	appid=$1
+	counter=0
+	printf "\n\n"
+	echo "-------------------------------------------------------------------------------------------------"	
+	echo "Here is the list of Containers launched for application: $appid"
+	echo "-------------------------------------------------------------------------------------------------"
+
+	printf "\n\n"
+
+	echo "Master Container On: "
+	echo "==================== "
+	printf "\n"
+	grep $appid yarn-mapr-resourcemanager-sat-node5.log | grep "MasterContainer:"
+
+
+	printf "\n\n"
+
+        echo "Containers: "
+        echo "=========== "	
+	printf "\n\n"
+
+	#echo "-------------------------------------------------------------------------------------------------"
+        #echo "|   Time stamp             |  Operation        |   Status   |                                     | Node                                   |"
+	#echo "-------------------------------------------------------------------------------------------------"
+
+	grep application_1489052008885_0015 yarn-mapr-resourcemanager-sat-node5.log | grep CONTAINERID | awk -F ' ' ' { print $1 " |" $2 "\t |" $7 "\t |" $10 "\t |" $12 "   |" }'
+
+
+	printf "\n\n"
+	echo "Containers and Allocated Nodes: "
+        echo "=============================== "
+        printf "\n"
+
+
+
+	echo "---------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+	grep "SchedulerNode: Assigned container" yarn-mapr-resourcemanager-sat-node5.log | grep 1489052008885_0015_01 | awk -F ' ' '{ print $1 $2 " | " $5 " | "  $7 " | " $10 $11 $12 $13 " | " $15 " | " $18}'
+
+	echo "---------------------------------------------------------------------------------------------------------------------------------------------------------"
+}
+
+#Execution starts here
+
+if [ $# -lt 2 ]
+then
+	
+	usage
+else
+
+	validate_id $1 $2
+	getContainers $1 $2	
+fi
+
---
+++ etc/analyzerEnv.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Adding Necessary Environment values
+
+export ANALZER_HOME=/home/mapr/myProjects/YarnApplicationAnalyzer
+export ANALYZER_CONF=$ANALZER_HOME/etc
+
