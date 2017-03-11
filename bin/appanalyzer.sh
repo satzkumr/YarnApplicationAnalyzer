ad1f282173aa0d8138e7d66ddcd3d12d98a74a05
satzkumr <mrsathishkumar12@gmail.com>
2017-03-11 18:44:25 +0530

    Commit for 0.02 with basic features

--- bin/appanalyzer.sh
+++ bin/appanalyzer.sh
@@ -34,15 +34,21 @@ validate_id()
 getContainers()
 {
 	appid=$1
-	containers=(`echo "$appid" | awk -F '_' '{print $2 "_" $3 "_0*"}'`)
+	containers=(`echo "$appid" | awk -F '_' '{print $2 "_" $3 "_0"}'`)
 	counter=0
 	printf "\n\n"
 	echo "-------------------------------------------------------------------------------------------------"	
 	echo "Here is the list of Containers launched for application: $appid"
 	echo "-------------------------------------------------------------------------------------------------"
 
-	printf "\n\n"
+	printf "\n"
+	echo "-------------------------------------------------------------------------------------------------"
+	echo "| Time Stamp           |   State  |          Applicatoin ID       | User | Queue name| Curr Apps|"
+	echo "-------------------------------------------------------------------------------------------------"
+	grep "in queue" $logFile | grep $appid | awk -F ' ' '{ print $1 $2 " | " $5 " | " $7 "|"$10 " | " $13 "  | " $NF }'
+	echo "-------------------------------------------------------------------------------------------------"
 
+	printf "\n"
 	echo "Master Container On: "
 	echo "==================== "
 	printf "\n"
@@ -69,9 +75,10 @@ getContainers()
 
 	echo "---------------------------------------------------------------------------------------------------------------------------------------------------------"
 
-	printf "\n"
+	containerIds=(`grep "SchedulerNode: Assigned container" $logFile | grep $containers | awk -F ' ' '{ print $7 "\n"}'`)
 	totalContainers=`grep "SchedulerNode: Assigned container" $logFile | grep $containers | awk -F ' ' '{ print $1 $2 " | " $5 " | "  $7 " | " $10 $11 $12 $13 " | " $15 " | " $18}' | wc -l`
 	
+	printf "\n"
 	printf "Total Number of Containers Allocated: $totalContainers"
 	printf "\n\n"
         echo "Released Containers and  Nodes: "
@@ -97,6 +104,22 @@ getContainers()
         echo "-----------------------------------------------------------------------------------------"
 	grep $appid  $logFile | grep "State change" | awk -F ' ' '{print $1 "|" $2 " | " $5 " | " $9 "-->" $11}'
 	echo "-----------------------------------------------------------------------------------------"
+
+	printf "\n\n"
+        echo "Container  State changes: "
+        echo "========================= "
+        printf "\n"	
+
+
+	for cid in `grep "SchedulerNode: Assigned container" $logFile | grep "$containers" | awk -F ' ' '{ print $7 }'`
+	do
+		echo "-----------------------------------------------------------------------------------------"
+		echo "$cid"; 
+		echo "-----------------------------------------------------------------------------------------"
+        	grep $cid $logFile  | grep "Container Transitioned" | grep "$containers" |  awk -F ' ' '{ print $1 "|" $2 " | " $5 " | " $9 " -->" $11 }'
+		printf "\n"
+	done;
+	
 }
 
 #Execution starts here
